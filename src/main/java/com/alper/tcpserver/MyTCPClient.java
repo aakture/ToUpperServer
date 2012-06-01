60af32b831de4e302620b97c400fb7e7a4597ba2
Alper Akture <alper.akture@gmail.com>
2012-06-01 09:52:30 -0700

    first commit

---
+++ src/main/java/com/alper/tcpserver/MyTCPClient.java
@@ -0,0 +1,35 @@
+package com.alper.tcpserver;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple client to test the toUpper server. 
+ * @author Alper Akture
+ *
+ */
+public class MyTCPClient {
+	private static final Logger log = LoggerFactory.getLogger(MyTCPClient.class);
+
+	public static void main(String[] args) throws Exception {
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("localhost", 6789);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		log.info("FROM SERVER: {}", modifiedSentence);
+		clientSocket.close();
+	}
+
+
+}
---
+++ src/main/java/com/alper/tcpserver/ToUpperServer.java
@@ -0,0 +1,70 @@
+package com.alper.tcpserver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A server that reads a line of text, and returns it uppercased. Run this as a java application,
+ * and then run the 
+ * @author aakture
+ *
+ */
+public class ToUpperServer {
+
+	private static final Logger log = LoggerFactory.getLogger(ToUpperServer.class);
+
+	public static void main(String[] args) throws Exception {
+
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+
+		while (true) {
+			log.info("waiting for connection");
+			Socket connectionSocket = welcomeSocket.accept();
+			ToUpperServer server = new ToUpperServer();
+			server.new MyHandler(connectionSocket).run();
+			
+
+		}
+	}
+
+	class MyHandler implements Runnable {
+		private Socket clientSocket;
+
+		public MyHandler(Socket socket) {
+			this.clientSocket = socket;
+		}
+
+		public void run() {
+			try {
+				log.info("conection accepted");
+
+				log.info("try readLine()...");
+				String clientSentence = readString(clientSocket);
+				log.info("Received: {}", clientSentence);
+				String capitalizedSentence = clientSentence.toUpperCase();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				writer.println(capitalizedSentence);
+				writer.flush();
+				writer.close();
+				
+				clientSocket.close();
+				log.info("sent {} to client", capitalizedSentence);
+			} catch (Exception ex) {
+				log.error("error", ex);
+			}
+		}
+
+		private String readString(Socket clientSocket) throws Exception {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			String message = reader.readLine();
+			return message;
+		}
+
+	}
+}
